3f3bd31db8fd5699e749333e2aec04d9f31e4a12
kunal2020-stack <kunal2115kumar@gmail.com>
2023-07-02 21:33:30 +0530

    if else condition code

---
+++ src/com/udemy/tutorial/kunal/if_else_condition.java
@@ -0,0 +1,17 @@
+package com.udemy.tutorial.kunal;
+
+public class if_else_condition {
+	public static void main(String[] args) {
+		int a=10;
+		if(a == 10) {
+			System.out.println("it is ");
+		}
+		else if(a<10) {
+			System.out.println("It is less than");
+		}
+		else {
+			System.out.println("It is greater than");
+		}
+	}
+
+}
