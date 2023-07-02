5ba869a74f1e36bd1e4df891d7d1e4b242229119
kunal2020-stack <kunal2115kumar@gmail.com>
2023-07-02 20:32:45 +0530

    Added first code for the while loop

---
+++ src/com/udemy/tutorial/kunal/While_Loop.java
@@ -0,0 +1,14 @@
+package com.udemy.tutorial.kunal;
+
+public class While_Loop {
+	public static void main (String args[]) {
+		System.out.println("While loop[");
+		int a=10;
+		int i=1;
+		while(i<=a) {
+			System.out.println("2 * "+i+" ="+" "+(2*i));
+			i+=1;
+		}
+	}
+
+}
